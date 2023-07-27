613027b5d8718b5deb11d9b174e8fc423654f77b
Christine Oh <Christineo4321@gmail.com>
2023-07-27 13:01:08 -0400

    Initial Commit

---
+++ Final project/Final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_projectApp.swift
+//  Final project
+//
+//  Created by Scholar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
